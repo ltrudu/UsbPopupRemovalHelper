02cdcf464bae7f749365ec288e4cb77af0dd582b
Trudu Laurent <laurent@trudu.com>
2023-09-29 10:48:30 +0200

    Added process control rule to get more flexilibity.

--- UsbPopupRemovalHelper/src/main/java/com/zebra/usbpopupremovalhelper/UsbPopupRemovalHelper.java
+++ UsbPopupRemovalHelper/src/main/java/com/zebra/usbpopupremovalhelper/UsbPopupRemovalHelper.java
@@ -1,85 +1,77 @@
 package com.zebra.usbpopupremovalhelper;
 
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.text.TextUtils;
 
-import java.util.Base64;
-
 public class UsbPopupRemovalHelper {
-    public static Signature apkCertificate = null;
 
-    public static void whitelistDeviceWithIDs(Context context, int vendorId, int productId, EDeviceClass deviceClass, IResultCallbacks callbackInterface) {
+    public static void processControlRuleXMLAlreadyHTMLEncoded(Context context, String controlRuleXMLAlreadyHTMLEncoded, IResultCallbacks iResultCallbacks)
+    {
+        processControlRule(context, controlRuleXMLAlreadyHTMLEncoded, true, iResultCallbacks);
+    }
+    public static void processRawControlRuleXML(Context context, String controlRuleXMLAlreadyHTMLEncoded, IResultCallbacks iResultCallbacks)
+    {
+        processControlRule(context, controlRuleXMLAlreadyHTMLEncoded, false, iResultCallbacks);
+    }
+
+    private static void processControlRule(Context context, String controlRuleXML, boolean isHtmlEncoded, IResultCallbacks iResultCallbacks)
+    {
+        String tmpControlRule = controlRuleXML;
         String profileName = "UsbMgr-1";
         String profileData = "";
         try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNING_CERTIFICATES);
-            String path = context.getApplicationInfo().sourceDir;
-            final String strPackageName = packageInfo.packageName;
 
-            // Use custom signature if it has been set by the user
-            Signature sig = UsbPopupRemovalHelper.apkCertificate;
-
-            // Let's check if we have a custom certificate
-            if (sig == null) {
-                // Nope, we will get the first apk signing certificate that we find
-                // You can copy/paste this snippet if you want to provide your own
-                // certificate
-                // TODO: use the following code snippet to extract your custom certificate if necessary
-                final Signature[] arrSignatures = packageInfo.signingInfo.getApkContentsSigners();
-                if (arrSignatures == null || arrSignatures.length == 0) {
-                    if (callbackInterface != null) {
-                        callbackInterface.onError("Error : Package has no signing certificates... how's that possible ?","");
-                        return;
-                    }
-                }
-                sig = arrSignatures[0];
+            if(iResultCallbacks != null)
+            {
+                iResultCallbacks.onDebugStatus("Unencoded control rule: " + tmpControlRule);
             }
 
-            /*
-             * Get the X.509 certificate.
-             */
-            final byte[] rawCert = sig.toByteArray();
+            if(!isHtmlEncoded) {
+                tmpControlRule = TextUtils.htmlEncode(tmpControlRule);
+                if (iResultCallbacks != null) {
+                    iResultCallbacks.onDebugStatus("HTML encoded control rule: " + tmpControlRule);
+                }
+            }
 
-            // Get the certificate as a base64 string
-            String encoded = Base64.getEncoder().encodeToString(rawCert);
-            if(callbackInterface != null)
+            profileData = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                    "<characteristic type=\"Profile\">" +
+                    "<parm name=\"ProfileName\" value=\"" + profileName + "\"/>" +
+                    "  <characteristic version=\"11.8\" type=\"UsbMgr\">\n" +
+                    "    <parm name=\"HostModePeripherals\" value=\"1\" />\n" +
+                    "    <parm name=\"ControlRules\" value=\"" + tmpControlRule + "\" />\n" +
+                    "  </characteristic>\n" +
+                    "</characteristic>";
+
+            ProfileManagerCommand profileManagerCommand = new ProfileManagerCommand(context);
+            profileManagerCommand.execute(profileData, profileName, iResultCallbacks);
+            //}
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (iResultCallbacks != null) {
+                iResultCallbacks.onError("Error on profile: " + profileName + "\nError:" + e.getLocalizedMessage() + "\nProfileData:" + profileData, "");
+            }
+        }
+
+    }
+
+    public static void whitelistDeviceWithIDs(Context context, int vendorId, int productId, EDeviceClass deviceClass, IResultCallbacks iResultCallbacks) {
+        String profileName = "UsbMgr-1";
+        String profileData = "";
+        try {
+            String strPackageName = PackageManagementHelper.getPackageName(context, iResultCallbacks);
+
+            String encoded = PackageManagementHelper.getSignature(context, iResultCallbacks);
+
+            if(iResultCallbacks != null)
             {
-                callbackInterface.onDebugStatus("Unencoded signature: " + encoded);
+                iResultCallbacks.onDebugStatus("Unencoded signature: " + encoded);
             }
 
             encoded = TextUtils.htmlEncode(encoded);
-            if(callbackInterface != null)
+            if(iResultCallbacks != null)
             {
-                callbackInterface.onDebugStatus("HTML encoded signature: " + encoded);
+                iResultCallbacks.onDebugStatus("HTML encoded signature: " + encoded);
             }
-            profileData =
-                    "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-                            "<characteristic type=\"Profile\">" +
-                            "<parm name=\"ProfileName\" value=\"" + profileName + "\"/>" +
-                            "  <characteristic version=\"11.8\" type=\"UsbMgr\">\n" +
-                            "    <parm name=\"HostModePeripherals\" value=\"1\" />\n" +
-                            "    <parm name=\"ControlRules\" value=\"&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?" +
-                            "&gt;&lt;popupsuppress&gt;"+
-                            "&lt;rule&gt;"+"" +
-                            "&lt;pkg&gt;" + strPackageName +
-                            "&lt;/pkg&gt;" +
-                            "&lt;cert&gt;" + encoded +
-                            "&lt;/cert&gt;" +
-                            "&lt;vid vendorid=&quot;" + vendorId +
-                            "&quot;&gt;&lt;pid&gt;" +productId +
-                            "&lt;/pid&gt;&lt;/vid&gt;&lt;/rule&gt;&lt;/popupsuppress&gt;"+
-                            "&lt;usbconfig mode=&quot;whitelist&quot;&gt;" +
-                            "&lt;class&gt;USB_CLASS_PRINTER&lt;/class&gt;" +
-                            //"&lt;vid vendorid=&quot;" + vendorId +
-                            //"&quot;&gt;&lt;pid&gt;"+ productId +"&lt;/pid&gt;&lt;/vid&gt;" +
-                            "&lt;/usbconfig&gt;"+
-                            "\" />\n" +
-                            "  </characteristic>\n"+
-                            "</characteristic>";
-
 
             profileData = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
                     "<characteristic type=\"Profile\">" +
@@ -91,12 +83,12 @@ public class UsbPopupRemovalHelper {
                     "</characteristic>";
 
             ProfileManagerCommand profileManagerCommand = new ProfileManagerCommand(context);
-            profileManagerCommand.execute(profileData, profileName, callbackInterface);
+            profileManagerCommand.execute(profileData, profileName, iResultCallbacks);
             //}
         } catch (Exception e) {
             e.printStackTrace();
-            if (callbackInterface != null) {
-                callbackInterface.onError("Error on profile: " + profileName + "\nError:" + e.getLocalizedMessage() + "\nProfileData:" + profileData, "");
+            if (iResultCallbacks != null) {
+                iResultCallbacks.onError("Error on profile: " + profileName + "\nError:" + e.getLocalizedMessage() + "\nProfileData:" + profileData, "");
             }
         }
     }
@@ -105,36 +97,7 @@ public class UsbPopupRemovalHelper {
         String profileName = "UsbMgr-1";
         String profileData = "";
         try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNING_CERTIFICATES);
-            String path = context.getApplicationInfo().sourceDir;
-            final String strPackageName = packageInfo.packageName;
-
-            // Use custom signature if it has been set by the user
-            Signature sig = UsbPopupRemovalHelper.apkCertificate;
-
-            // Let's check if we have a custom certificate
-            if (sig == null) {
-                // Nope, we will get the first apk signing certificate that we find
-                // You can copy/paste this snippet if you want to provide your own
-                // certificate
-                // TODO: use the following code snippet to extract your custom certificate if necessary
-                final Signature[] arrSignatures = packageInfo.signingInfo.getApkContentsSigners();
-                if (arrSignatures == null || arrSignatures.length == 0) {
-                    if (callbackInterface != null) {
-                        callbackInterface.onError("Error : Package has no signing certificates... how's that possible ?","");
-                        return;
-                    }
-                }
-                sig = arrSignatures[0];
-            }
-
-            /*
-             * Get the X.509 certificate.
-             */
-            final byte[] rawCert = sig.toByteArray();
-
-            // Get the certificate as a base64 string
-            String encoded = Base64.getEncoder().encodeToString(rawCert);
+            String encoded = PackageManagementHelper.getSignature(context, callbackInterface);
 
             profileData =
                     "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
